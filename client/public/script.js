55bd821cd22b64aefb73e6af7e3f7a5e2adca944
Astrahalora <astrahalora@gmail.com>
2023-02-21 09:27:33 +0200

    Add autocomplete and fetch

--- client/public/script.js
+++ client/public/script.js
@@ -15,31 +15,77 @@ const rootElement = document.querySelector("#root");
 
 rootElement.insertAdjacentHTML(
   "afterbegin",
-  `<div id="pick_city">
-    <label>Search City: 
-        <input type="text">
-    </label>
-</div>`
+  `<form autocomplete="off">
+      <label for="input">Pick a City:</label>
+      <div>            
+          <input type="text" id="input">
+      </div>
+  </form>`
 );
 
-const enteredCities = () => {
-  return cities
-    .map((city) => {
-      if (city.includes(inputElement.value)) {
-        return `<li>${city}</li>`;
+const inputElement = document.getElementById('input');
+
+const autocompleteAndDisplay = (input, list) => {
+  const closeList = () => {
+      const suggestions = document.getElementById('suggestions');
+      const section = document.querySelector('section');
+      if (suggestions) {
+          suggestions.parentNode.removeChild(suggestions);
+          section.remove();
       };
-    })
-    .join("");
+  };
+
+  input.addEventListener('input', function () {
+      //Close the existing list if it is open
+      closeList();
+
+      //If the input is empty, exit the function
+      if (!this.value)
+          return;
+
+      //Create a suggestions <div> and add it to the element containing the input field
+      const suggestions = document.createElement('div');
+      suggestions.setAttribute('id', 'suggestions');
+      this.parentNode.appendChild(suggestions);
+
+      //Iterate through all entries in the list and find matches
+      list.map(listItem => {
+          if(listItem.toUpperCase().includes(this.value.toUpperCase())) {
+              let suggestion = document.createElement('div');
+              suggestion.innerHTML = listItem;
+
+              suggestion.addEventListener('click', function() {
+              input.value = this.innerHTML;
+
+              let currentUrl = `http://api.weatherapi.com/v1/current.json?key=b54d788ad89546eeaf2133644232002&q=${input.value}&aqi=no`
+              fetchData(currentUrl);
+
+              closeList();
+              });
+              suggestions.appendChild(suggestion);
+          };
+      });
+  });
+};
+autocompleteAndDisplay(inputElement, cities);
+
+const makePage = (data) => {
+  rootElement.insertAdjacentHTML("beforeend", `
+  <section>
+      <h1>${data.location.name}</h1>
+  </section>`)
 };
 
-const inputElement = document.querySelector("input");
-inputElement.addEventListener("input", () => {
-  if (inputElement.value.length > 0) {
-    rootElement.insertAdjacentHTML(
-      "beforeend",
-      `<ul>
-      ${enteredCities()}
-      </ul>`
-    );
+const fetchData = async (url) => {
+  try {
+      const initialData = await fetch(url);
+      const data = await initialData.json();
+      makePage(data);
+  } catch (error) {
+      error = 'There was an error'
+      rootElement.insertAdjacentHTML("beforeend", `
+      <section>
+           <h1>${error}</h1>
+      </section>`);
   };
-});
+};
